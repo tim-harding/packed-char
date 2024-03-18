c566611cf6ce314b84f6079c85daec4ce82d3073
Tim Harding <Tim@TimHarding.co>
2024-03-18 10:35:00 -0700

    Documentation and tests for U22

--- src/lib.rs
+++ src/lib.rs
@@ -23,6 +23,9 @@
 //! valid char because it falls in the surrogate range. We use this to
 //! distinguish what the packed char contains.
 
+mod u22;
+pub use u22::{U22FromU32Error, U22};
+
 use std::fmt::{self, Debug, Formatter};
 
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
@@ -37,7 +40,7 @@ impl PackedChar {
     const TRAILING: u32 = Self::SURROGATE_LOW.trailing_zeros(); // 11
     const TRAILING_MASK: u32 = !(u32::MAX << Self::TRAILING);
     const CHAR_MASK: u32 = !Self::LEADING_MASK;
-    const MAX_U22_LEADING: u32 = U22::MAX.as_u32().leading_zeros();
+    const MAX_U22_LEADING: u32 = U22::MAX.leading_zeros();
 
     pub const fn from_char(c: char) -> Self {
         Self(c as u32)
@@ -95,47 +98,6 @@ impl TryFrom<u32> for PackedChar {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct U22(u32);
-
-impl U22 {
-    pub const MAX: Self = Self(!(u32::MAX << 22));
-
-    pub const fn from_u32(n: u32) -> Result<Self, U22FromU32Error> {
-        if n > Self::MAX.as_u32() {
-            Err(U22FromU32Error(n))
-        } else {
-            Ok(Self(n))
-        }
-    }
-
-    pub const unsafe fn from_u32_unchecked(n: u32) -> Self {
-        Self(n)
-    }
-
-    pub const fn as_u32(self) -> u32 {
-        self.0
-    }
-}
-
-impl TryFrom<u32> for U22 {
-    type Error = U22FromU32Error;
-
-    fn try_from(n: u32) -> Result<Self, Self::Error> {
-        Self::from_u32(n)
-    }
-}
-
-impl From<U22> for u32 {
-    fn from(u22: U22) -> Self {
-        u22.0
-    }
-}
-
-#[derive(Debug, thiserror::Error, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-#[error("{0} exceeds U22::MAX ({})", U22::MAX.as_u32())]
-pub struct U22FromU32Error(u32);
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum PackedCharContents {
     Char(char),
     U22(U22),
@@ -156,14 +118,7 @@ mod tests {
 
     #[test]
     fn gets_back_indices() {
-        let test_indices = [
-            U22::MAX.as_u32(),
-            0x3FFFFFu32,
-            0,
-            69,
-            420,
-            0b1010101010101010101010,
-        ];
+        let test_indices = [U22::MAX, 0x3FFFFFu32, 0, 69, 420, 0b1010101010101010101010];
         for i in test_indices {
             let packed = PackedChar::try_from(i).unwrap();
             assert_eq!(
@@ -175,7 +130,7 @@ mod tests {
 
     #[test]
     fn fails_out_of_bounds_indices() {
-        let test_indices = [U22::MAX.as_u32() + 1, 0b10101010101010101010101010101010];
+        let test_indices = [U22::MAX + 1, 0b10101010101010101010101010101010];
         for i in test_indices {
             let packed = PackedChar::try_from(i);
             assert_eq!(packed, Err(U22FromU32Error(i)));
---
+++ src/u22.rs
@@ -0,0 +1,113 @@
+use std::{
+    borrow::Borrow,
+    error::Error,
+    fmt::{self, Display, Formatter},
+    ops::Deref,
+};
+
+/// A 22-bit unsigned integer.
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct U22(u32);
+
+impl Display for U22 {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl U22 {
+    /// The largest value that can be expressed by this type
+    pub const MAX: u32 = !(u32::MAX << 22);
+
+    /// Creates a new 22-bit integer from the given 32-bit integer if it is small enough to fit.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use packed_char::{U22, U22FromU32Error};
+    /// assert_eq!(U22::from_u32(42).map(U22::as_u32), Ok(42));
+    /// assert_eq!(U22::from_u32(U22::MAX).map(U22::as_u32), Ok(U22::MAX));
+    /// assert_eq!(U22::from_u32(U22::MAX + 1), Err(U22FromU32Error(U22::MAX + 1)));
+    /// ```
+    pub const fn from_u32(n: u32) -> Result<Self, U22FromU32Error> {
+        if n > Self::MAX {
+            Err(U22FromU32Error(n))
+        } else {
+            Ok(Self(n))
+        }
+    }
+
+    /// Creates a new 22-bit integer from the given 32-bit integer.
+    ///
+    /// # Safety
+    ///
+    /// The provided integer must be no greater than [`U22::MAX`].
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use packed_char::U22;
+    /// let u22 = unsafe { U22::from_u32_unchecked(42) };
+    /// assert_eq!(u22.as_u32(), 42);
+    /// ```
+    pub const unsafe fn from_u32_unchecked(n: u32) -> Self {
+        Self(n)
+    }
+
+    /// Gets the 22-bit integer as a 32-bit integer.
+    ///
+    /// # Examples
+    ///
+    /// # use packed_char::U22;
+    /// let u22 = U22::from_u32(42).unwrap();
+    /// assert_eq!(u22.as_ref(), 42);
+    pub const fn as_u32(self) -> u32 {
+        self.0
+    }
+}
+
+impl TryFrom<u32> for U22 {
+    type Error = U22FromU32Error;
+
+    fn try_from(n: u32) -> Result<Self, Self::Error> {
+        Self::from_u32(n)
+    }
+}
+
+impl From<U22> for u32 {
+    fn from(u22: U22) -> Self {
+        u22.0
+    }
+}
+
+impl AsRef<u32> for U22 {
+    fn as_ref(&self) -> &u32 {
+        &self.0
+    }
+}
+
+impl Borrow<u32> for U22 {
+    fn borrow(&self) -> &u32 {
+        &self.0
+    }
+}
+
+impl Deref for U22 {
+    type Target = u32;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+/// Error type for 32-bit to 22-bit integer conversion.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct U22FromU32Error(pub u32);
+
+impl Display for U22FromU32Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{} exceeds U22::MAX", self.0)
+    }
+}
+
+impl Error for U22FromU32Error {}
